0b549a5a09d9c70c274d009118d6b2fd6bde348b
bangyadiii <triadilaksamana@gmail.com>
2022-05-22 21:04:53 +0700

    add nav item

---
+++ app/src/main/java/com/example/coursera/ui/dashboard/BookViewModel.java
@@ -0,0 +1,19 @@
+package com.example.coursera.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class BookViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public BookViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
