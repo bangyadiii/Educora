2cca3e11b2d1211d6ef9bc7a1e0fb4167fb6d948
bangyadiii <triadilaksamana@gmail.com>
2022-06-08 22:53:03 +0700

    fix layout book detail and fix some error

--- app/src/main/java/com/example/coursera/model/Course.java
+++ app/src/main/java/com/example/coursera/model/Course.java
@@ -1,14 +1,29 @@
 package com.example.coursera.model;
 
+import com.google.firebase.firestore.DocumentId;
+
 public class Course {
+    @DocumentId
+    private String id;
     private String title;
     private String description;
+    private String icon_url;
 
     public Course() {}
 
-    public Course(String title, String description) {
+    public Course(String id, String title, String description, String icon_url) {
+        this.id = id;
         this.title = title;
         this.description = description;
+        this.icon_url = icon_url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -25,5 +40,13 @@ public class Course {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getIcon_url() {
+        return icon_url;
+    }
+
+    public void setIcon_url(String icon_url) {
+        this.icon_url = icon_url;
     }
 }
--- app/src/main/java/com/example/coursera/ui/home/ProgressCourseAdapter.java
+++ app/src/main/java/com/example/coursera/ui/home/ProgressCourseAdapter.java
@@ -1,25 +1,39 @@
 package com.example.coursera.ui.home;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import androidx.navigation.NavAction;
+import androidx.navigation.NavDirections;
 import androidx.navigation.NavOptions;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.coursera.R;
 import com.example.coursera.databinding.ProgressClassItemBinding;
 import com.example.coursera.model.Course;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class ProgressCourseAdapter extends FirestoreRecyclerAdapter<Course, ProgressCourseAdapter.CourseItemHolder> {
@@ -58,7 +72,8 @@ public class ProgressCourseAdapter extends FirestoreRecyclerAdapter<Course, Prog
 //                    .setPopUpTo(R.id.navigation_home, true)
 //                    .build();
             Log.d("model", model.toString());
-            Navigation.findNavController(context.findViewById(R.id.nav_host_fragment_activity_main)).navigate(R.id.action_navigation_home_to_detail_course);
+            NavDirections action = (NavDirections) HomeFragmentDirections.actionNavigationHomeToDetailCourse(model.getId());
+            Navigation.findNavController(context.findViewById(R.id.nav_host_fragment_activity_main)).navigate(action);
 
         });
     }
@@ -78,9 +93,32 @@ public class ProgressCourseAdapter extends FirestoreRecyclerAdapter<Course, Prog
         }
         public void bind(Course course){
             this.course = course;
+            Log.d("course", course.getIcon_url());
             binding.tvCourseTitle.setText(course.getTitle());
             binding.tvDescription.setText(course.getDescription());
+            StorageReference mStorageReference= FirebaseStorage.getInstance().getReference().child(course.getIcon_url());
+            try{
+//            String image_url;
+                final File localFile = File.createTempFile("course_icon","png");
+                mStorageReference.getFile(localFile)
+                        .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>(){
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot){
+                                Toast.makeText(context,"Picture Retrieved",Toast.LENGTH_SHORT).show();
+                                Bitmap bitmap= BitmapFactory.decodeFile(localFile.getAbsolutePath());
 
+                                Glide.with(context).load(bitmap)
+                                        .into(binding.ivCourseIcon);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(context, "Error Occurred", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
 
         }
 
